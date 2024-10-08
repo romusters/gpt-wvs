e3e6a7863ba95ad04f76482506a518067e17da4d
robert.musters <robert.musters@cgi.com>
2024-10-08 11:14:33 +0200

    Multi page.

---
+++ streamlit_pages/homepage.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Homepage met uitleg")
+st.markdown("""
+            Links staan de verschillende paginas voor de verschillende experimenten.
+            
+            De sidebar kan ook ingeklapt worden waardoor het scherm groter wordt.
+            """)
